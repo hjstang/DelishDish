966aeb460c930fa710bcb68daa20711c79242dbf
Ida Merete Enholm <ida.enholm@me.com>
2019-11-24 19:12:23 +0100

    added prettier

--- backend/firebaseApi.js
+++ backend/firebaseApi.js
@@ -1,44 +1,56 @@
-import { firestore } from './firebaseConfig';
+import { firestore } from "./firebaseConfig";
 
-export async function getUser(){
-    try {
-        const data = [];
-        await firestore.collection('users').get().then((querySnapshot) => {
-            querySnapshot.forEach(function (doc) {
-                if (doc.exists) {
-                    data.push({id: doc.id, name: doc.data().name, surname: doc.data().surname, email: doc.data().email})
-                }
-            })
+export async function getUser() {
+  try {
+    const data = [];
+    await firestore
+      .collection("users")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(function(doc) {
+          if (doc.exists) {
+            data.push({
+              id: doc.id,
+              name: doc.data().name,
+              surname: doc.data().surname,
+              email: doc.data().email
+            });
+          }
         });
-        console.log(data);
-    } catch ({message}) {
-        console.log(message);
-    }
-    return "hello";
+      });
+    console.log(data);
+  } catch ({ message }) {
+    console.log(message);
+  }
+  return "hello";
 }
 
-export async function getRecipe(id){
-    try {
-        const recipe = [];
-        await firestore.collection('recipes').doc(id).get().then((docSnapshot) => {
-            const doc = docSnapshot.data();
-            recipe.push({
-                id,
-                title: doc.title,
-                description: doc.description,
-                difficulty: doc.difficulty,
-                servings: doc.servings,
-                sourceUrl: doc.sourceUrl,
-                favorited: doc.favorited,
-                cuisine: doc.cuisine,
-                dishTypes: doc.dishTypes,
-                healthTypes: doc.healthTypes,
-                ingredients: doc.ingredients,
-                imageUrl: doc.imageUrl
-            });
+export async function getRecipe(id) {
+  try {
+    const recipe = [];
+    await firestore
+      .collection("recipes")
+      .doc(id)
+      .get()
+      .then(docSnapshot => {
+        const doc = docSnapshot.data();
+        recipe.push({
+          id,
+          title: doc.title,
+          description: doc.description,
+          difficulty: doc.difficulty,
+          servings: doc.servings,
+          sourceUrl: doc.sourceUrl,
+          favorited: doc.favorited,
+          cuisine: doc.cuisine,
+          dishTypes: doc.dishTypes,
+          healthTypes: doc.healthTypes,
+          ingredients: doc.ingredients,
+          imageUrl: doc.imageUrl
         });
-        return recipe[0];
-    } catch (e) {
-        console.log(e)
-    }
+      });
+    return recipe[0];
+  } catch (e) {
+    console.log(e);
+  }
 }
--- views/search.js
+++ views/search.js
@@ -1,37 +1,32 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "../components/LoginScreen";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-class Search extends Component{
-
-    render(){
-        const { auth } = this.props;
-        return (
-            <View style={styles.container}>
-                {auth.uid ?
-                    <Text>User logged in</Text>
-                    :
-                    <LoginScreen />
-                }
-            </View>
-        );
-    }
+class Search extends Component {
+  render() {
+    const { auth } = this.props;
+    return (
+      <View style={styles.container}>
+        {auth.uid ? <Text>User logged in</Text> : <LoginScreen />}
+      </View>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth
-    }
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 export default connect(mapStateToProps)(Search);
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-    },
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1
+  }
 });
