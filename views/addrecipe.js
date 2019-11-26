c1cf696239ffeb88159209aacf187ce972bdd2e5
Ida Merete Enholm <ida.enholm@me.com>
2019-11-26 10:48:01 +0100

    implemented actionSheet and ImagePicker

--- views/addrecipe.js
+++ views/addrecipe.js
@@ -1,12 +1,92 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Picker } from "react-native";
 import { connect } from "react-redux";
 import LoginScreen from "../components/LoginScreen";
 import { createRecipe } from "../store/actions/recipeActions";
-import {compose} from "redux";
-import {firestoreConnect} from "react-redux-firebase";
+import { compose } from "redux";
+import { firestoreConnect } from "react-redux-firebase";
+import * as ImagePicker from "expo-image-picker";
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import { connectActionSheet } from "@expo/react-native-action-sheet";
 
 class AddRecipe extends Component {
+  uploadImage = async (uri, imageName) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    let ref = firebase
+      .storage()
+      .ref()
+      .child(this.props.auth.uid + "/" + imageName);
+    return ref.put(blob);
+  };
+
+  _onOpenActionSheet = () => {
+    const options = ["Take a picture", "Choose from Library", "Cancel"];
+    const cancelButtonIndex = 2;
+
+    this.props.showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex
+      },
+      async buttonIndex => {
+        if (buttonIndex == 0) {
+          await this.onTakePicturePress();
+        } else if (buttonIndex == 1) {
+          await this.onChooseLibraryPress();
+        }
+      }
+    );
+  };
+
+  onTakePicturePress = async () => {
+    const { status } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.CAMERA_ROLL
+    );
+
+    if (status !== "granted") {
+      alert(
+        "Sorry, we need camera and camera roll permission to make this work!"
+      );
+    }
+
+    let result = await ImagePicker.launchCameraAsync();
+
+    if (!result.cancelled) {
+      {
+        this.uploadImage(result.uri, "test-image2")
+          .then(() => {
+            console.log("Image upload success");
+          })
+          .catch(err => {
+            console.log("Image upload failed", err);
+          });
+      }
+    }
+  };
+
+  onChooseLibraryPress = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status !== "granted") {
+      alert("Sorry, we need camera roll permission to make this work!");
+    }
+    let result = await ImagePicker.launchImageLibraryAsync();
+
+    if (!result.cancelled) {
+      this.this
+        .uploadImage(result.uri, "test-image3")
+        .then(() => {
+          console.log("image library upload success");
+        })
+        .catch(err => {
+          console.log("Image from library upload failed", err);
+        });
+    }
+  };
+
   render() {
     const { auth, defaultValues } = this.props;
     // defaultValues is containing all the predefined values such as measures, dishTypes etc.
@@ -32,6 +112,10 @@ class AddRecipe extends Component {
       <View style={styles.container}>
         {auth.uid ? (
           <View>
+            <Button
+              title="Choose image..."
+              onPress={() => this._onOpenActionSheet()}
+            />
             <Text>User logged in</Text>
             <Button
               title="Add Recipe"
@@ -59,20 +143,22 @@ export const mapDispatchToProps = dispatch => {
   };
 };
 
+const ConnectedAddRecipe = connectActionSheet(AddRecipe);
+
 export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
-    firestoreConnect(props => {
-      if (!props.auth.uid) {
-        return [];
+  connect(mapStateToProps, mapDispatchToProps),
+  firestoreConnect(props => {
+    if (!props.auth.uid) {
+      return [];
+    }
+    return [
+      {
+        collection: "defaultValues",
+        storeAs: "defaultValues"
       }
-      return [
-        {
-          collection: "defaultValues",
-          storeAs: "defaultValues",
-        }
-      ];
-    })
-)(AddRecipe);
+    ];
+  })
+)(ConnectedAddRecipe);
 
 const styles = StyleSheet.create({
   container: {
